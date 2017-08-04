48fbe6c885f515f56e0b763e0298e172246099f5
Mator <why101@msn.com>
2017-08-04 01:18:06 -0700

    created main state and controller

---
+++ src/Views/main.js
@@ -0,0 +1,48 @@
+export default function(ngapp, xelib, remote) {
+    ngapp.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider.state('base.main', {
+            templateUrl: 'partials/main.html',
+            controller: 'mainController',
+            url: '/main'
+        });
+    }]);
+
+    ngapp.controller('mainController', function ($scope, $rootScope, $timeout, spinnerFactory, xelibService, layoutService) {
+        $scope.loaded = false;
+        $scope.log = xelib.GetMessages();
+        $scope.spinnerOpts = spinnerFactory.defaultOptions;
+        xelibService.printGlobals();
+
+        // load default layout
+        layoutService.loadDefaultLayout($scope);
+
+        // scope functions
+        $scope.toggleSettingsModal = function(visible) {
+            $scope.showSettingsModal = visible;
+        };
+
+        $scope.getLoadingMessage = function() {
+            $scope.loadingMessage = $scope.log.split('\n').slice(-2)[0];
+        };
+
+        $scope.checkIfLoaded = function() {
+            $scope.log = $scope.log + xelib.GetMessages();
+            $scope.getLoadingMessage();
+            if (xelib.GetLoaderDone()) {
+                console.log($scope.log);
+                $scope.loaded = true;
+            } else {
+                $timeout($scope.checkIfLoaded, 250);
+            }
+        };
+
+        // terminate xelib when application is done
+        window.onbeforeunload = function(e) {
+            if (remote.app.forceClose) return;
+            //$scope.toggleSaveModal(true);
+            e.returnValue = true;//false;
+        };
+
+        $scope.checkIfLoaded();
+    });
+}
