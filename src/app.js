53200cbc7146d7ac889e3af26625dc31726d9a6d
Mator <why101@msn.com>
2017-08-03 22:28:49 -0700

    hooked angular services, filters, directives, and factories up

--- src/app.js
+++ src/app.js
@@ -4,6 +4,17 @@ import jetpack from 'fs-jetpack'; // module loaded from npm
 import fh from './helpers/fileHelpers.js';
 import env from './env';
 import xelib from './xelib.js';
+import settingsService from './Services/settingsService.js';
+import xelibService from './Services/xelibService.js';
+import profileService from './Services/profileService.js';
+import formUtils from './Services/formUtils.js';
+import spinnerFactory from './Factories/spinnerFactory.js';
+import listViewFactory from './Factories/listViewFactory.js';
+import profilesModal from './Directives/profilesModal.js';
+import loadOrderModal from './Directives/loadOrderModal.js';
+import settingsModal from './Directives/settingsModal.js';
+import hexFilter from './Filters/hexFilter.js';
+import profileValidFilter from './Filters/profileValidFilter.js';
 import baseView from './Views/base.js';
 import startView from './Views/start.js';
 
@@ -32,6 +43,25 @@ ngapp.run(['$rootScope', '$state', function ($rootScope, $state) {
         }
     });
 }]);
+
+// SERVICES
+xelibService(ngapp, xelib);
+profileService(ngapp, xelib, fileHelpers);
+formUtils(ngapp);
+settingsService(ngapp, fileHelpers);
+
+// FACTORIES
+spinnerFactory(ngapp);
+listViewFactory(ngapp);
+
+// FILTERS
+hexFilter(ngapp);
+profileValidFilter(ngapp);
+
+// DIRECTIVES
+profilesModal(ngapp, fileHelpers);
+loadOrderModal(ngapp, xelib);
+settingsModal(ngapp, fileHelpers);
 
 // VIEWS
 baseView(ngapp, remote);
