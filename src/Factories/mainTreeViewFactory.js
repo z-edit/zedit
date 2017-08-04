7e4d0314ade2dc598ae9d7b2380fc9d262ec31a6
Mator <why101@msn.com>
2017-08-04 01:17:34 -0700

    created main tree view factory

---
+++ src/Factories/mainTreeViewFactory.js
@@ -0,0 +1,27 @@
+export default function(ngapp, xelib) {
+    ngapp.service('mainTreeViewFactory', function() {
+        let factory = this;
+
+        this.releaseChildren = function(node) {
+            node.children.forEach(function(child) {
+                if (child.children) factory.releaseChildren(child);
+                xelib.Release(child);
+            });
+        };
+
+        this.destroy = function(view) {
+            factory.releaseChildren(view.data.tree);
+        };
+
+        this.new = function() {
+            return {
+                name: 'mainTreeView',
+                data: {
+                    tabLabel: 'Tree View',
+                    tree: {}
+                },
+                destroy: factory.destroy
+            }
+        };
+    });
+};
