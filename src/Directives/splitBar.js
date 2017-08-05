81936e38d5121cfd9ce56a6bdf2e4691d58ad0a0
Mator <why101@msn.com>
2017-08-04 22:24:00 -0700

    changed vars to let in splitBar directive link function

--- src/Directives/splitBar.js
+++ src/Directives/splitBar.js
@@ -6,22 +6,22 @@ export default function(ngapp) {
             scope: true,
             link: function(scope, element) {
                 // helper variables
-                var htmlElement = document.documentElement;
-                var container = element[0].parentElement;
-                var vertical = container.classList.contains('vertical');
-                var targetDimension = vertical ? 'height' : 'width';
-                var clientLabel = vertical ? 'clientY' : 'clientX';
-                var offsetLabel = vertical ? 'offsetTop' : 'offsetLeft';
-                var sizeLabel = vertical ? 'offsetHeight' : 'offsetWidth';
-                var moving = false;
+                let htmlElement = document.documentElement;
+                let container = element[0].parentElement;
+                let vertical = container.classList.contains('vertical');
+                let targetDimension = vertical ? 'height' : 'width';
+                let clientLabel = vertical ? 'clientY' : 'clientX';
+                let offsetLabel = vertical ? 'offsetTop' : 'offsetLeft';
+                let sizeLabel = vertical ? 'offsetHeight' : 'offsetWidth';
+                let moving = false;
 
                 // event handlers
-                var handleMouseMove = function(e) {
-                    var paneElement = element[0].previousElementSibling;
-                    var percentage = (e[clientLabel] - paneElement[offsetLabel]) / container[sizeLabel];
+                let handleMouseMove = function(e) {
+                    let paneElement = element[0].previousElementSibling;
+                    let percentage = (e[clientLabel] - paneElement[offsetLabel]) / container[sizeLabel];
                     paneElement.style[targetDimension] = Math.min(percentage, 1.0).toPercentage(1);
                 };
-                var handleMouseDown = function(e) {
+                let handleMouseDown = function(e) {
                     // only trigger when left mouse button is pressed
                     if (e.button !== 0 && e.type !== 'touchstart') return;
                     moving = true;
@@ -30,7 +30,7 @@ export default function(ngapp) {
                     e.preventDefault();
                     e.stopPropagation();
                 };
-                var handleMouseUp = function(e) {
+                let handleMouseUp = function(e) {
                     if (!moving) return;
                     moving = false;
                     htmlElement.removeEventListener('mousemove', handleMouseMove);
