b4f81a0f72f88b7650300a65df49b603a52b9d8e
Mator <why101@msn.com>
2017-08-04 22:22:47 -0700

    panes now initialize their size properly

--- src/Directives/pane.js
+++ src/Directives/pane.js
@@ -12,5 +12,14 @@ export default function(ngapp) {
 
     ngapp.controller('paneController', function ($scope, $element) {
         angular.inheritScope($scope, 'pane');
+
+        // helper variables
+        let paneElement = $element[0];
+        let container = paneElement.parentElement;
+        let parentVertical = container.classList.contains('vertical');
+        let dimension = parentVertical ? 'height' : 'width';
+
+        // initialize pane size
+        paneElement.style[dimension] = $scope[dimension];
     });
 }
