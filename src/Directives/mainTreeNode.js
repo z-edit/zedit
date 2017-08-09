ca49352d4b1c172f6fb38f1ef62426a8869390d1
Mator <why101@msn.com>
2017-08-08 21:44:20 -0700

    removed unnecessary mainTreeNode directive

--- src/Directives/mainTreeNode.js
+++
@@ -1,8 +0,0 @@
-export default function(ngapp) {
-    ngapp.directive('mainTreeNode', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'directives/mainTreeNode.html'
-        }
-    });
-}
