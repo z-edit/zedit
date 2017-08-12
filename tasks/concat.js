5b7df66ed168f3f6a677b28105a86acee8aed925
Mator <why101@msn.com>
2017-08-12 15:13:55 -0700

    updated rollup plugin concat

--- tasks/concat.js
+++ tasks/concat.js
@@ -1,35 +1,59 @@
 var jetpack = require('fs-jetpack');
 var path = require('path');
+var os = require('os');
+var MagicString = require('magic-string');
+
+var options = {};
+
+var ensureTrailingNewLine = function(code) {
+  if (!code.endsWith(os.EOL)) code += os.EOL;
+  return code;
+};
 
 var load = function(path) {
-    if (!jetpack.exists(path)) console.log('ERROR: File not found at "' + path + '"');
-    return jetpack.read(path);
+    if (!jetpack.exists(path)) console.log(`ERROR: File not found at "${path}"`);
+    let code = ensureTrailingNewLine(jetpack.read(path));
+    const magicStr = new MagicString(code);
+    concatFiles(magicStr, code, path);
+    return magicStr.toString();
 };
 
 var loadTree = function(path) {
     return jetpack.find(path, { matching: '*.js' }).reduce(function(code, filePath) {
-        return code  + (code != '' && '\n\n' || '') + load(filePath);
+        return code + load(filePath);
     }, '');
 };
 
-module.exports = function(options) {
-    options = options || {};
+var concatFiles = function(magicStr, code, id) {
+    var dir = path.dirname(id),
+        regex = /\/\/= concat(_tree)? ([^\n\r]+)/gi,
+        changes = false;
+    code.replace(regex, function(match, tree, target, index) {
+        if (options.debug) console.log(`rollup-plugin-concat: processing "${match}" in "${id}"`);
+        changes = true;
+        var targetPath = path.join(dir, target);
+        var insertedCode = (tree ? loadTree(targetPath) : load(targetPath));
+        magicStr.overwrite(index, index + match.length, insertedCode);
+        return insertedCode;
+    });
+    return changes;
+};
+
+module.exports = function(opts) {
+    if (opts) options = opts;
     return {
         name: 'concat',
         transform: function(code, id) {
-            var dir = path.dirname(id);
-            var regex = /\/\/= concat(_tree)? ([^\n\r]+)/gi;
-            var result = code.replace(regex, function(match, tree, target) {
-                if (options.debug) {
-                    console.log('rollup-plugin-concat: processing "' + match + '" in "' + id + '"');
-                }
-                let targetPath = path.join(dir, target);
-                return (tree ? loadTree(targetPath) : load(targetPath)) + '\n';
-            });
-            return {
-                code: result,
-                map: {mappings: '' }
-            };
+            var magicStr = new MagicString(code);
+            var changes = concatFiles(magicStr, code, id);
+            if (changes) {
+              return {
+                  code: magicStr.toString(),
+                  map: magicStr.generateMap({ hires: true })
+              };
+            }
+            
+            return null; // tell rollup to discard this result
         }
     };
 };
