c25f98e1a74501418818cd0f7f0fdc71e1a7bd9c
Mator <why101@msn.com>
2017-08-12 15:19:57 -0700

    created autofocus directive

---
+++ src/javascripts/Directives/autofocus.js
@@ -0,0 +1,8 @@
+ngapp.directive('ngAutofocus', function($timeout) {
+    return {
+        restrict: 'A',
+        link : function(scope, element) {
+            $timeout(function() { element[0].focus(); });
+        }
+    }
+});
