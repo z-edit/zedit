b7d402e7f9ecc494152bab4384f6f97de42e05bd
Mator <why101@msn.com>
2017-08-13 20:46:43 -0700

    added some additional function to mainTreeNodeService

--- src/javascripts/Services/mainTreeNodeService.js
+++ src/javascripts/Services/mainTreeNodeService.js
@@ -1,8 +1,34 @@
 ngapp.service('mainTreeNodeService', function() {
-    this.buildNodeFunctions = function(scope) {
+    this.buildFunctions = function(scope) {
+        // helper variables
         let ctClasses = ['ct-unknown', 'ct-ignored', 'ct-not-defined', 'ct-identical-to-master', 'ct-only-one', 'ct-hidden-by-mod-group', 'ct-master', 'ct-conflict-benign', 'ct-override', 'ct-identical-to-master-wins-conflict', 'ct-conflict-wins', 'ct-conflict-loses'];
         let caClasses = ['ca-unknown', 'ca-only-one', 'ca-no-conflict', 'ca-conflict-benign', 'ca-override', 'ca-conflict', 'ca-conflict-critical'];
 
+        // helper functions
+        let reExpandNode = function(node) {
+            let newNode = getNodeForElement(node.handle);
+            if (newNode) {
+                scope.getNodeData(newNode);
+                scope.expandNode(newNode);
+            }
+        };
+
+        let reSelectNode = function(node, scroll) {
+            let newNode = getNodeForElement(node.handle);
+            if (newNode) {
+                scope.selectSingle(newNode, true, true, false);
+                if (scroll) scope.scrollToNode(newNode, true);
+            }
+        };
+
+        let freeHandles = function(nodes1, nodes2) {
+            let a = [];
+            nodes1.forEach((n) => a.contains(n.handle) || a.push(n.handle));
+            nodes2.forEach((n) => a.contains(n.handle) || a.push(n.handle));
+            a.forEach((handle) => xelib.Release(handle));
+        };
+
+        // scope functions
         scope.getNodeClass = function(node) {
             let classes = [];
             //if (xelib.GetModified(node.handle)) classes.push('modified');
@@ -12,6 +38,26 @@ ngapp.service('mainTreeNodeService', function() {
                 classes.push(caClasses[xelib.ConflictAll(node.handle)]);
             }
             node.class = classes.join(' ');
+        };
+
+        scope.reloadNodes = function() {
+            let start = Date.now();
+            scope.reloading = true;
+            let oldExpandedNodes = [];
+            let oldSelectedNodes = scope.selectedNodes.slice();
+            scope.tree.forEach(function(node) {
+                if (node.expanded) {
+                    oldExpandedNodes.push(node);
+                } else if (!node.selected) {
+                    xelib.Release(node.handle);
+                }
+            });
+            scope.clearSelection(true);
+            scope.buildTree();
+            oldExpandedNodes.forEach((n) => reExpandNode(n));
+            oldSelectedNodes.forEach((n, i, a) => reSelectNode(n, i == a.length - 1));
+            freeHandles(oldExpandedNodes, oldSelectedNodes);
+            console.log(`Rebuilt tree (${scope.tree.length} nodes) in ${Date.now() - start}ms`);
         };
 
         scope.getChildrenCount = function(node) {
