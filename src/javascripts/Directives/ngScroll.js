601afdc4dcce8df4cf5eddc1151ce42082463380
Mator <why101@msn.com>
2017-08-16 16:07:07 -0700

    created ngScroll directive

---
+++ src/javascripts/Directives/ngScroll.js
@@ -0,0 +1,13 @@
+ngapp.directive('ngScroll', function($parse) {
+    return {
+        restrict: 'A',
+        compile : function($element, attr) {
+            let fn = $parse(attr['ngScroll']);
+            return function ngEventHandler(scope, element) {
+                element.on('scroll', function(event) {
+                    scope.$apply(() => fn(scope, {$event: event}));
+                });
+            }
+        }
+    }
+});
