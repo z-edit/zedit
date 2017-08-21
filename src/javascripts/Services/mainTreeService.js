2d1faec3b87a9a4699ced41b4c78a8c374fb97fa
Mator <why101@msn.com>
2017-08-21 15:22:35 -0700

    moved functions from mainTreeView to mainTreeService

--- src/javascripts/Services/mainTreeService.js
+++ src/javascripts/Services/mainTreeService.js
@@ -22,6 +22,51 @@ ngapp.service('mainTreeService', function($timeout, mainTreeViewFactory) {
         };
 
         // scope functions
+        scope.buildColumns = function() {
+            scope.columns = scope.allColumns.filter((column) => { return column.enabled; });
+            let width = scope.columns.reduce(function(width, c) {
+                if (c.width) width += parseInt(c.width.slice(0, -1));
+                return width;
+            }, 0);
+            if (width > 100) {
+                let defaultWidth = Math.floor(100 / scope.columns.length) + '%';
+                scope.columns.slice(0, -1).forEach((column) => column.width = defaultWidth);
+            }
+            scope.resizeColumns();
+        };
+
+        scope.buildTree = function() {
+            xelib.SetSortMode(scope.sort.column, scope.sort.reverse);
+            scope.tree = xelib.GetElements(0, '', true).map(function(handle) {
+                return scope.buildNode(handle, -1);
+            });
+        };
+
+        scope.resolveNode = function(path) {
+            let node = undefined;
+            path.split('\\').forEach(function(part) {
+                let handle = xelib.GetElement(node ? node.handle : 0, `${part}`);
+                if (part !== 'Child Group') {
+                    node = scope.getNodeForElement(handle);
+                    if (!node) throw `Failed to resolve node "${part}" in path "${path}"`;
+                    if (!node.has_data) scope.getNodeData(node);
+                    if (!node.expanded) scope.expandNode(node);
+                }
+            });
+            return node;
+        };
+
+        scope.navigateToElement = function(handle) {
+            let node = scope.resolveNode(xelib.LongPath(handle));
+            if (node) {
+                scope.clearSelection(true);
+                scope.selectSingle(node, true, true, false);
+                $timeout(function() {
+                    scope.scrollToNode(node, true);
+                });
+            }
+        };
+        
         scope.getNodeClass = function(node) {
             let classes = [];
             //if (xelib.GetModified(node.handle)) classes.push('modified');
