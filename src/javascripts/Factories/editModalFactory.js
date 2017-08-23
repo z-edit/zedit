c8961fdba4ab033f90c424820c1b2986f074aba2
Mator <why101@msn.com>
2017-08-23 15:03:08 -0700

    created editModalFactory

---
+++ src/javascripts/Factories/editModalFactory.js
@@ -0,0 +1,43 @@
+ngapp.service('editModalFactory', function() {
+    this.renameFile = function(node, scope) {
+        return {
+            title: 'Rename File',
+            editType: 'string',
+            maxLength: 64,
+            initialValue: xelib.Name(node.handle),
+            isValid: (value) => { return value.length > 0; },
+            callback: (newValue) => {
+                xelib.RenameFile(node.handle, newValue);
+                scope.$root.$broadcast('nodeUpdated', node);
+            }
+        };
+    };
+
+    this.changeFileAuthor = function(node, scope) {
+        return {
+            title: 'Change File Author',
+            editType: 'string',
+            maxLength: 255,
+            initialValue: xelib.GetAuthor(node.handle),
+            isValid: () => { return true },
+            callback: (newValue) => {
+                xelib.SetAuthor(node.handle, newValue);
+                scope.$root.$broadcast('nodeUpdated', node);
+            }
+        };
+    };
+
+    this.changeFileDescription = function(node, scope) {
+        return {
+            title: 'Change File Description',
+            editType: 'text',
+            maxLength: 255,
+            initialValue: xelib.GetDescription(node.handle),
+            isValid: () => { return true },
+            callback: (newValue) => {
+                xelib.SetDescription(node.handle, newValue);
+                scope.$root.$broadcast('nodeUpdated', node);
+            }
+        };
+    };
+});
