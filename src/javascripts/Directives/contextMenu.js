a8d36f597a219e88d929a711b568b41a5995dfd1
Mator <why101@msn.com>
2017-08-23 13:46:34 -0700

    fixed nested context menu positioning

--- src/javascripts/Directives/contextMenu.js
+++ src/javascripts/Directives/contextMenu.js
@@ -1,43 +1,45 @@
-ngapp.directive('contextMenu', function($timeout) {
+ngapp.directive('contextMenu', function($timeout, htmlHelpers) {
     return {
         restrict: 'E',
         templateUrl: 'directives/contextMenu.html',
         scope: {
             items: '=',
-            offset: '=?'
+            offset: '=?',
+            nested: '=?'
         },
         controller: 'contextMenuController',
         link: function(scope, element) {
-            let e = element[0];
-            if (scope.offset) {
-                e.style.top = '0';
-                e.style.left = '0';
-                e.style.visibility = 'hidden';
-                $timeout(function() {
-                    if (scope.offset.top + e.offsetHeight > window.innerHeight) {
-                        scope.offset.top = window.innerHeight - e.offsetHeight - 16;
-                    }
-                    if (scope.offset.left + e.offsetWidth > window.innerWidth) {
+            let e = element[0],
+                table = e.firstElementChild;
+            if (!scope.offset) {
+                scope.offset = { top: e.offsetTop - 8, left: e.offsetLeft };
+            }
+            e.style.top = '0';
+            e.style.left = '0';
+            e.style.visibility = 'hidden';
+            $timeout(function() {
+                if (e.offsetHeight > window.innerHeight) {
+                    scope.offset.top = 45;
+                    scope.showScroll = true;
+                    e.style['max-height'] = window.innerHeight - 50 + 'px';
+                } else if (scope.offset.top + e.offsetHeight > window.innerHeight) {
+                    scope.offset.top = window.innerHeight - e.offsetHeight - 16;
+                }
+                if (scope.offset.left + e.offsetWidth > window.innerWidth) {
+                    if (scope.nested) {
+                        let parentMenu = htmlHelpers.findParent(e, function(element) {
+                            return element.tagName === 'CONTEXT-MENU';
+                        });
+                        scope.offset.left -= parentMenu.offsetWidth + e.offsetWidth;
+                    } else {
                         scope.offset.left = window.innerWidth - e.offsetWidth - 6;
                     }
-                    e.style.top = scope.offset.top + 'px';
-                    e.style.left = scope.offset.left + 'px';
-                    e.style.visibility = 'visible';
-                });
-            } else {
-                e.style.top = '0';
-                e.style.left = '0';
-                e.style.visibility = 'hidden';
-                scope.offset = { top: -2, left: 16 };
-                $timeout(function() {
-                    // TODO: move up/down as necessary
-                    // TODO: move to left side of parent if necessary
-                    // TODO: enable scrolling if necessary
-                    e.style.top = scope.offset.top + 'px';
-                    e.style.left = scope.offset.left + 'px';
-                    e.style.visibility = 'visible';
-                });
-            }
+                }
+                e.style.top = scope.offset.top + 'px';
+                e.style.left = scope.offset.left + 'px';
+                e.style.visibility = 'visible';
+                table.focus();
+            });
         }
     }
 });
