8507ff8ae176f0bb6ebabdbc16d2e18056e72440
Mator <why101@msn.com>
2017-08-23 15:55:06 -0700

    fixed bug with not checking for uneditable value types before opening edit modal

--- src/javascripts/Services/recordTreeElementService.js
+++ src/javascripts/Services/recordTreeElementService.js
@@ -1,10 +1,13 @@
 ngapp.service('recordTreeElementService', function() {
     this.buildFunctions = function(scope) {
+        let uneditableValueTypes = [xelib.vtUnknown, xelib.vtArray, xelib.vtStruct];
+
         scope.addElement = function(node, index) {
             // TODO
         };
 
         scope.editElement = function(node, index) {
+            if (uneditableValueTypes.contains(node.value_type)) return;
             scope.targetNode = node;
             scope.targetIndex = index - 1;
             scope.toggleEditModal(true);
