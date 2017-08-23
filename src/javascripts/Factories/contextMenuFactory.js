424ff73ac9d2caa0e797466d9b2a54e6ac5598bc
Mator <why101@msn.com>
2017-08-23 13:47:57 -0700

    created main tree view context menu items

--- src/javascripts/Factories/contextMenuFactory.js
+++ src/javascripts/Factories/contextMenuFactory.js
@@ -1,10 +1,28 @@
 ngapp.service('contextMenuFactory', function() {
+    let divider = {
+        visible: (scope, items) => { return items.length > 0 && !items.last().divider; },
+        build: (scope, items) => items.push({ divider: true })
+    };
+
+    let testNodes = function(nodes, testFn) {
+        return nodes.reduce(function (b, node) {
+            return b && testFn(node);
+        }, true);
+    };
+
     this.mainTreeItems = [{
-        available: (items, node) => { return xelib.GetCanAdd(node.handle); },
-        build: (items, node, scope) => {
-            let addList = xelib.GetAddList(node.handle);
+        id: 'Add',
+        visible: (scope) => {
+            let node = scope.selectedNodes.last();
+            return node.element_type !== xelib.etMainRecord && xelib.GetCanAdd(node.handle);
+        },
+        build: (scope, items) => {
+            let node = scope.selectedNodes.last(),
+                addList = xelib.GetAddList(node.handle),
+                isFile = node.element_type === xelib.etFile;
             items.push({
-                label: 'Add',
+                label: `Add ${isFile ? 'Group' : 'Record'}`,
+                hotkey: isFile ? 'Ctrl+G' : (addList.length == 1 && 'Ctrl+N'),
                 disabled: !addList.length,
                 children: addList.map(function(item) {
                     return {
@@ -15,12 +33,158 @@ ngapp.service('contextMenuFactory', function() {
             });
         }
     }, {
-        available: (items, node) => { return xelib.GetIsRemoveable(node.handle); },
-        build: (items, node, scope) => {
-            items.push({
-                label: 'Remove',
-                callback: () => scope.removeElement(node)
+        id: 'Delete',
+        visible: (scope) => {
+            return testNodes(scope.selectedNodes, function(node) {
+                return xelib.GetIsRemoveable(node.handle);
             });
+        },
+        build: (scope, items) => {
+            items.push({
+                label: 'Delete',
+                hotkey: 'Del',
+                callback: () => scope.deleteElements()
+            });
+        }
+    }, {
+        id: 'Refactor',
+        visible: (scope) => {
+            let nodes = scope.selectedNodes, node = nodes.last();
+            return node.element_type === xelib.etFile && xelib.GetIsEditable(node.handle) ||
+                testNodes(nodes, function(node) {
+                    return node.element_type === xelib.etMainRecord && xelib.GetIsEditable(node.handle);
+                });
+        },
+        build: (scope, items) => {
+            let node = scope.selectedNodes.last(),
+                children = [];
+            if (node.element_type === xelib.etFile) {
+                children.push({
+                    label: 'Rename File',
+                    hotkey: 'F2, Alt+Shift+R',
+                    callback: () => scope.changeFileName(node)
+                }, {
+                    label: 'Change Author',
+                    hotkey: 'Alt+Shift+A',
+                    callback: () => scope.changeFileAuthor(node)
+                }, {
+                    label: 'Change Description',
+                    hotkey: 'Alt+Shift+D',
+                    callback: () => scope.changeFileDescription(node)
+                });
+            } else if (node.element_type === xelib.etMainRecord) {
+                children.push({
+                    label: 'Rename',
+                    hotkey: 'F2, Alt+Shift+R',
+                    callback: () => scope.toggleRefactorModal(true, 'Name')
+                }, {
+                    label: 'Change EditorIDs',
+                    hotkey: 'Alt+Shift+E',
+                    callback: () => scope.toggleRefactorModal(true, 'EditorID')
+                }, {
+                    label: 'Change FormIDs',
+                    hotkey: 'Alt+Shift+F',
+                    callback: () => scope.toggleRefactorModal(true, 'FormID')
+                });
+            }
+            items.push({
+                label: 'Refactor',
+                children: children
+            });
+        }
+    }, {
+        id: 'Enable Editing',
+        visible: (scope) => {
+            return testNodes(scope.selectedNodes, function(node) {
+                return node.element_type === xelib.etFile && !xelib.GetIsEditable(node.handle);
+            });
+        },
+        build: (scope, items) => {
+            items.push({
+                label: 'Enable Editing',
+                hotkey: 'Ctrl+E',
+                callback: () => scope.enableEditing(node)
+            })
+        }
+    }, divider, {
+        id: 'Automate',
+        visible: () => { return true; },
+        build: (scope, items) => {
+            items.push({
+                label: 'Automate',
+                hotkey: 'Ctrl+M',
+                callback: () => scope.toggleAutomateModal()
+            })
+        }
+    }, {
+        id: 'Advanced Search',
+        visible: () => { return true; },
+        build: (scope, items) => {
+            items.push({
+                label: 'Advanced Search',
+                hotkey: 'Ctrl+Shift+F',
+                callback: () => scope.toggleSearchModal()
+            })
+        }
+    }, {
+        id: 'Open',
+        visible: (scope) => {
+            return testNodes(scope.selectedNodes, function(node) {
+                return node.element_type === xelib.etMainRecord;
+            });
+        },
+        build: (scope, items) => {
+            items.push({
+                label: 'Open in Record View',
+                hotkey: 'Enter',
+                callback: () => scope.onNodeDoubleClick({}, node)
+            })
+        }
+    }, divider, {
+        id: 'Copy',
+        visible: () => { return true; },
+        build: (scope, items) => {
+            let node = scope.selectedNodes.last();
+            items.push({
+                label: 'Copy',
+                hotkey: 'Ctrl+C',
+                disabled: !node,
+                callback: () => scope.copyNodes()
+            })
+        }
+    }, {
+        id: 'Copy Path',
+        visible: () => { return true; },
+        build: (scope, items) => {
+            let node = scope.selectedNodes.last();
+            items.push({
+                label: 'Copy Path',
+                hotkey: 'Ctrl+Shift+C',
+                disabled: !node,
+                callback: () => scope.copyNodePaths()
+            })
+        }
+    }, {
+        id: 'Paste',
+        visible: () => { return true },
+        build: (scope, items) => {
+            items.push({
+                label: 'Paste',
+                hotkey: 'Ctrl+V',
+                disabled: !scope.canPaste(),
+                callback: () => scope.paste()
+            })
+        }
+    }, {
+        id: 'Paste as Override',
+        visible: () => { return true; },
+        build: (scope, items) => {
+            items.push({
+                label: 'Paste as Override',
+                hotkey: 'Ctrl+Shift+V',
+                disabled: !scope.canPaste(true),
+                callback: () => scope.paste(true)
+            })
         }
     }];
 });
