019c903d547218fed67056704aa49b5ad75d5f82
Mator <why101@msn.com>
2017-08-25 18:30:50 -0700

    added record tree element service functions for adding and deleting elements

--- src/javascripts/Services/recordTreeElementService.js
+++ src/javascripts/Services/recordTreeElementService.js
@@ -1,9 +1,24 @@
-ngapp.service('recordTreeElementService', function() {
+ngapp.service('recordTreeElementService', function(errorService) {
     this.buildFunctions = function(scope) {
         let uneditableValueTypes = [xelib.vtUnknown, xelib.vtArray, xelib.vtStruct];
 
+        // scope functions
         scope.addElement = function(node, index) {
-            // TODO
+            errorService.try(function() {
+                let handle = node.handles[index],
+                    record = index ? scope.overrides[index - 1] : scope.record;
+                if (handle) {
+                    // append element to array
+                    xelib.AddElement(handle, '.');
+                } else {
+                    // create element in struct
+                    handle = node.parent ? node.parent.handles[index] : record;
+                    if (!handle) return; // this should never happen
+                    xelib.AddElement(handle, node.label);
+                }
+                scope.reload(); // TODO? This is kind of greedy, but it's simple
+                scope.$root.$broadcast('recordUpdated', record);
+            });
         };
 
         scope.editElement = function(node, index) {
@@ -11,6 +26,23 @@ ngapp.service('recordTreeElementService', function() {
             scope.targetNode = node;
             scope.targetIndex = index - 1;
             scope.toggleEditModal(true);
+        };
+
+        scope.deleteElement = function(node) {
+            errorService.try(function() {
+                let handle = node.handles[scope.focusedIndex - 1];
+                xelib.RemoveElement(handle);
+                xelib.Release(handle);
+            });
+        };
+
+        scope.deleteElements = function() {
+            scope.selectedNodes.forEach(scope.deleteElement);
+            scope.reload(); // TODO? This is kind of greedy, but it's simple
+        };
+
+        scope.canPaste = function(asOverride) {
+            return false;
         };
     }
 });
