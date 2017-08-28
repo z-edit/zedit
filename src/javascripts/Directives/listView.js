5fe4f4661ebd14a1b8dd1ade6d49bac6486753e6
Mator <why101@msn.com>
2017-08-27 21:41:23 -0700

    list view now supports reordering elements through drag and drop

--- src/javascripts/Directives/listView.js
+++ src/javascripts/Directives/listView.js
@@ -9,6 +9,7 @@ ngapp.directive('listView', function() {
             scope.reorderable = attrs.hasOwnProperty('reorderable');
             scope.items = scope.$eval(attrs.items);
             scope.listClass = attrs.listClass;
+            scope.listType = attrs.listType;
         }
     }
 });
@@ -16,6 +17,12 @@ ngapp.directive('listView', function() {
 ngapp.controller('listViewController', function($scope) {
     // helper variables
     let prevIndex = undefined;
+
+    // helper functions
+    let removeClasses = function(element) {
+        element.classList.remove('insert-after');
+        element.classList.remove('insert-before');
+    };
 
     // scope functions
     $scope.clearSelection = function() {
@@ -59,4 +66,41 @@ ngapp.controller('listViewController', function($scope) {
         e.stopImmediatePropagation();
         e.preventDefault();
     };
+
+    $scope.onItemDrag = function(index) {
+        if (!$scope.reorderable) return;
+        $scope.$root.dragData = {
+            source: $scope.listType,
+            index: index
+        };
+        return true;
+    };
+
+    $scope.onItemDragOver = function(e) {
+        if (!$scope.reorderable) return;
+        let dragData = $scope.$root.dragData;
+        if (!dragData || dragData.source !== $scope.listType) return;
+        let after = e.clientX > e.target.offsetHeight / 2;
+        e.target.classList[after ? 'add' : 'remove']('insert-after');
+        e.target.classList[after ? 'remove' : 'add']('insert-before');
+        return true;
+    };
+
+    $scope.onItemDragLeave = (e) => removeClasses(e.target);
+
+    $scope.onItemDrop = function(e, index) {
+        if (!$scope.reorderable) return;
+        let dragData = $scope.$root.dragData;
+        if (!dragData || dragData.source !== $scope.listType) return;
+        removeClasses(e.target);
+        if (dragData.index === index) return;
+        let after = e.clientX > e.target.offsetHeight / 2,
+            movedItem = $scope.items.splice(dragData.index, 1)[0];
+        $scope.items.splice(index + after, 0, movedItem);
+        return true;
+    };
+
+    // event handling
+    $scope.$on('clearSelection', $scope.clearSelection);
+    $scope.$on('keyPress', (e, event) => $scope.onKeyPress(event));
 });
