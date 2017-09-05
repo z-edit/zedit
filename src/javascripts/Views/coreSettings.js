02be271178e14da146872d360ac975fc6bc0e1e5
Mator <why101@msn.com>
2017-09-04 17:23:26 -0700

    created and registered core settings controller

---
+++ src/javascripts/Views/coreSettings.js
@@ -0,0 +1,17 @@
+let coreSettingsController = function($scope, themeService) {
+    // initialization
+    $scope.themes = themeService.getThemes();
+    $scope.theme = $scope.themes.find(function(theme) {
+        return theme.filename === $scope.globalSettings.theme;
+    });
+
+    // scope functions
+    $scope.themeChanged = function() {
+        $scope.globalSettings.theme = $scope.theme.filename;
+        $scope.$emit('setTheme', $scope.theme.filename);
+    };
+};
+
+ngapp.run(function(controllerRegistry) {
+    controllerRegistry.register('coreSettingsController', coreSettingsController);
+});
