02f282a207c176b2c6eafed14fe2ef6f3a0246b3
JD Smith <jdsmith2816@gmail.com>
2017-09-08 01:55:27 -0500

    load xelib in main then remote into renderer

--- src/javascripts/background.js
+++ src/javascripts/background.js
@@ -9,10 +9,13 @@ import { app, Menu } from 'electron';
 import { devMenuTemplate } from './menu/dev_menu_template';
 import { editMenuTemplate } from './menu/edit_menu_template';
 import createWindow from './helpers/window';
+import xelib from './xelib';
 
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
 import env from './env';
+
+global.xelib = xelib;
 
 var mainWindow;
 
