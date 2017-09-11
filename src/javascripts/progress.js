d6c191c59e6bc222ebec6633c0c295e8b25d59a2
Mator <why101@msn.com>
2017-09-11 16:33:18 -0700

    created progress javascript file with basic angular app and ipc listeners

---
+++ src/javascripts/progress.js
@@ -0,0 +1,51 @@
+import { ipcRenderer } from 'electron';
+import './polyfills';
+
+// angular app initialization
+const ngapp = angular.module('progress', ['vs-repeat', 'luegg.directives', 'angularSpinner']);
+
+//= concat ./Factories/spinnerFactory.js
+//= concat ./Services/modalService.js
+//= concat ./Directives/progressBar.js
+//= concat ./Directives/progressModal.js
+//= concat ./Directives/loader.js
+
+ngapp.run(function($rootScope, spinnerFactory) {
+    $rootScope.progress = {
+        determinate: false,
+        message: '...'
+    };
+
+    $rootScope.whiteOpts = spinnerFactory.whiteOptions;
+
+    $rootScope.$on('unfocus-modal', () => {
+        if ($rootScope.progress.canClose) ipcRenderer.send('hide-progress');
+    });
+
+    ipcRenderer.on('set-progress', (e, payload) => {
+        if (!payload) return;
+        $rootScope.$applyAsync(() => $rootScope.progress = payload);
+    });
+
+    ipcRenderer.on('progress-title', (e, payload) => {
+        $rootScope.$applyAsync(() => $rootScope.progress.title = payload);
+    });
+
+    ipcRenderer.on('progress-message', (e, payload) => {
+        $rootScope.$applyAsync(() => $rootScope.progress.message = payload);
+    });
+
+    ipcRenderer.on('add-progress', (e, payload) => {
+        $rootScope.$applyAsync(() => $rootScope.progress.current += payload);
+    });
+
+    ipcRenderer.on('log-message', (e, payload) => {
+        if (!$rootScope.progress.log) return;
+        $rootScope.$applyAsync(() => {
+            $rootScope.progress.log.push({
+                message: payload[0],
+                level: payload[1]
+            });
+        });
+    });
+});
