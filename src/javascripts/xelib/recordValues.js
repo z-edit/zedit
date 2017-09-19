089abfca3f21bb1974a2b042e357379d8debd246
Mator <why101@msn.com>
2017-09-19 00:51:49 -0700

    adjusted/refactored some code in xelib recordValues

--- src/javascripts/xelib/recordValues.js
+++ src/javascripts/xelib/recordValues.js
@@ -3,29 +3,25 @@ import './common.js';
 // RECORD VALUE METHODS
 Object.assign(xelib, {
     EditorID: function(id) {
-        if (!xelib.HasElement(id, 'EDID')) return;
         return xelib.GetValue(id, 'EDID');
     },
     FullName: function(id) {
-        if (!xelib.HasElement(id, 'FULL')) return;
         return xelib.GetValue(id, 'FULL');
     },
     Translate: function(id, vector) {
         let position = xelib.GetElement(id, 'DATA\\Position');
         ['X', 'Y', 'Z'].forEach(function(coord) {
-            if (vector.hasOwnProperty(coord)) {
-                let newValue = xelib.GetFloatValue(position, coord) + vector[coord];
-                xelib.SetFloatValue(position, coord, newValue);
-            }
+            if (!vector.hasOwnProperty(coord)) return;
+            let newValue = xelib.GetFloatValue(position, coord) + vector[coord];
+            xelib.SetFloatValue(position, coord, newValue);
         });
     },
     Rotate: function(id, vector) {
         let rotation = xelib.GetElement(id, 'DATA\\Rotation');
         ['X', 'Y', 'Z'].forEach(function(coord) {
-            if (vector.hasOwnProperty(coord)) {
-                let newValue = xelib.GetFloatValue(rotation, coord) + vector[coord];
-                xelib.SetFloatValue(rotation, coord, newValue);
-            }
+            if (!vector.hasOwnProperty(coord)) return;
+            let newValue = xelib.GetFloatValue(rotation, coord) + vector[coord];
+            xelib.SetFloatValue(rotation, coord, newValue);
         });
     },
     GetRecordFlag: function(id, name) {
