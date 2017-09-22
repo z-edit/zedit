ce9db58cfcb1b4f42ebc61edb55dc914be30a034
Mator <why101@msn.com>
2017-09-21 20:34:55 -0700

    fleshed out installedThemesController

--- src/javascripts/Views/manageExtensions/installedThemes.js
+++ src/javascripts/Views/manageExtensions/installedThemes.js
@@ -1,26 +1,33 @@
-ngapp.controller('installedThemes', function($scope, extensionService) {
+ngapp.controller('installedThemesController', function($scope, extensionService) {
     $scope.themes = extensionService.getInstalledThemes();
 
     // scope functions
-    $scope.updateTheme = function(theme) {
-
-    };
-
     $scope.applyTheme = function(theme) {
-
+        $scope.themes.forEach((theme) => theme.applied = false);
+        theme.applied = true;
+        $scope.$emit('setTheme', theme.filename);
     };
 
     $scope.uninstallTheme = function(theme) {
-
+        if (theme.applied) {
+            let otherTheme = $scope.themes.find((t) => { return t !== theme});
+            $scope.applyTheme(otherTheme);
+        }
+        fs.jetpack.remove(`themes\\${theme.filename}`);
+        $scope.themes.remove(theme);
     };
 
-    $scope.updateAllThemes = function() {
-        $scope.themes.filter(function(theme) {
-            return theme.hasUpdate;
-        }).forEach($scope.updateTheme);
-    };
-
-    $scope.installCustomTheme = function() {
-
+    $scope.installTheme = function() {
+        let themeFile = fh.selectFile('Select a theme archive or css file.', '', [
+            { name: 'ZIP Archive', extensions: ['zip'] },
+            { name: 'CSS File', extensions: ['css'] }
+        ]);
+        if (!themeFile) return;
+        try {
+            extensionService.installTheme(themeFile);
+            $scope.themes = extensionService.getInstalledThemes(true);
+        } catch (x) {
+            alert(`Error installing theme:\r\n${x.stack}`);
+        }
     };
 });
