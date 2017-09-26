66a2b3fddd82c348500b8797cbd67649189f2bf4
Mator <why101@msn.com>
2017-09-26 13:35:25 -0700

    added profiles documentation page, added list of supported games to game modes page

--- src/javascripts/Directives/supportedGames.js
+++
@@ -1,7 +0,0 @@
-ngapp.directive('supportedGames', function() {
-    return {
-        restrict: 'E',
-        template: '<ul><li ng-repeat="game in games">{{::game.name}}</li></ul>',
-        link: (scope) => scope.games = xelib.games
-    }
-});
