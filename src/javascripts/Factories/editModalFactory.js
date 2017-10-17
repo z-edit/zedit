f3baea52aa107feacc885d69206043ba9d7dd472
Mator <why101@msn.com>
2017-10-17 08:40:49 -0700

    added filename validation and support for custom callback to editModalFactory.addFile

--- src/javascripts/Factories/editModalFactory.js
+++ src/javascripts/Factories/editModalFactory.js
@@ -41,17 +41,17 @@ ngapp.service('editModalFactory', function() {
         });
     };
 
-    this.addFile = function(scope) {
+    this.addFile = function(scope, callback) {
         scope.$emit('openModal', 'edit', {
             title: 'Add File',
             editType: 'string',
             maxLength: 64,
             initialValue: 'New File.esp',
-            isValid: () => { return true },
-            callback: (fileName) => {
-                xelib.AddFile(fileName);
-                scope.$root.$broadcast('fileAdded');
-            }
+            isValid: (value) => { return !xelib.HasElement(0, value) },
+            callback: callback || ((fileName) => {
+                xelib.Release(xelib.AddFile(fileName));
+                scope.$root.$broadcast('reloadGUI');
+            })
         });
     };
 });
