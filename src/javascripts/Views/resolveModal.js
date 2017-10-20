94cf99d25b0ffcffad77ee717afea93eb421ee97
Mator <why101@msn.com>
2017-10-19 20:24:34 -0700

    created resolveModal

---
+++ src/javascripts/Views/resolveModal.js
@@ -0,0 +1,48 @@
+ngapp.controller('resolveModalController', function ($scope, errorTypeFactory, pluginErrorService, hotkeyService) {
+    // HELPER FUNCTIONS
+    let prepareResolutions = function() {
+        $scope.resolutions = pluginErrorService.getResolutions($scope.error);
+        $scope.selectedIndex = $scope.resolutions.indexOf($scope.error.resolution);
+    };
+
+    let setError = function() {
+        if ($scope.errorIndex >= $scope.errorsToResolve.length) {
+            $scope.$emit('closeModal');
+            return;
+        }
+        $scope.error = $scope.errorsToResolve[$scope.errorIndex];
+        $scope.group = $scope.errorGroups[$scope.error.group];
+        prepareResolutions();
+    };
+
+    // SCOPE FUNCTIONS
+    $scope.selectResolution = function(resolution) {
+        $scope.error.resolution = resolution;
+        $scope.nextError();
+    };
+
+    $scope.nextError = function() {
+        $scope.errorIndex++;
+        setError();
+    };
+
+    $scope.previousError = function() {
+        $scope.errorIndex--;
+        setError();
+    };
+
+    $scope.selectResolution = function(e) {
+        let n = e.keyCode - 49; // 49 -> keycode for 1
+        if (n >= 0 && n < $scope.resolutions.length) {
+            $scope.selectResolution($scope.resolutions[n]);
+        }
+    };
+
+    // SET UP HOTKEYS
+    hotkeyService.buildOnKeyDown($scope, 'onKeyDown', 'resolveModal');
+
+    // INITIALIZATION
+    $scope.errorGroups = errorTypeFactory.errorTypes();
+    $scope.errorIndex = 0;
+    setError();
+});
