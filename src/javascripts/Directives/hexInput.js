df15cb6ce2679cff681eb6d03ae3667cb0ac4ded
Mator <why101@msn.com>
2017-10-27 17:18:28 -0700

    created hex input directive

---
+++ src/javascripts/Directives/hexInput.js
@@ -0,0 +1,42 @@
+ngapp.directive('hexInput', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            model: '=',
+            maxLength: '=?'
+        },
+        templateUrl: 'directives/hexInput.html',
+        controller: 'hexInputController'
+    }
+});
+
+ngapp.controller('hexInputController', function($scope) {
+    Object.defaults($scope, {
+        maxLength: 8
+    });
+
+    // helper functions
+    let isHexKey = function(key) {
+        return (key > 47 && key < 58) || (key > 64 && key < 71);
+    };
+
+    let focusNextSpan = function(e) {
+        let nextSpan = e.srcElement.nextElementSibling;
+        nextSpan ? nextSpan.focus() : e.srcElement.blur();
+    };
+
+    // scope functions
+    $scope.onKeyDown = function(e, index) {
+        if (!isHexKey(e.keyCode)) return;
+        $scope.hexStr[index] = String.fromCharCode(e.keyCode).toUpperCase();
+        focusNextSpan(e);
+    };
+
+    // two-way data binding for model
+    $scope.updateModel = function() {
+        $scope.model = parseInt($scope.hexStr, 16);
+    };
+    $scope.$watch('model', function() {
+        $scope.hexStr = xelib.Hex($scope.model, $scope.maxLength);
+    });
+});
