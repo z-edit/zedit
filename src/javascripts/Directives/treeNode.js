be967166ace077e2ccd96b3ae790c0ab2b70e0e4
Mator <why101@msn.com>
2017-11-09 21:30:54 -0800

    further optimization of the tree view, still have issues with function optimization it seems

---
+++ src/javascripts/Directives/treeNode.js
@@ -0,0 +1,29 @@
+let initNodeFn = function(scope, element) {
+    let el = element[0];
+
+    // event listeners
+    el.addEventListener('mousedown', function(e) {
+        scope.onNodeMouseDown(e, scope.node);
+    });
+
+    el.addEventListener('doubleclick', function(e) {
+        scope.onNodeDoubleClick(e, scope.node);
+    });
+
+    scope.$watch('node.selected', function(newVal) {
+        el.classList[newVal ? 'add' : 'remove']('selected');
+    });
+
+    // initialize node data
+    if (scope.node.hasData) return;
+    scope.getNodeData(scope.node);
+    el.className = `${el.className} ${scope.node.class}`;
+};
+
+ngapp.directive('treeNode', function() {
+    return {
+        restrict: 'A',
+        priority: 450,
+        link: initNodeFn
+    }
+});
