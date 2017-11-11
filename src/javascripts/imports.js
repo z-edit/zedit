ef0c98cf2d92c43034fda063e406d7827b2ca7e9
narukaze <bigguy132l@gmail.com>
2017-11-11 11:27:15 -0500

    Moved all the angular imports into a new file

---
+++ src/javascripts/imports.js
@@ -0,0 +1,8 @@
+import 'angular';
+import 'angular-ui-router';
+import 'ui-router-extras';
+import 'angular-spinner';
+import 'angular-color-picker';
+import 'angular-elastic-input';
+import 'angular-marked';
+import 'angular-vs-repeat';
