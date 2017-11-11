c7f3e8c1016045949928dc2c503a4c75ed11dfc3
narukaze <bigguy132l@gmail.com>
2017-11-11 13:40:25 -0500

    Refactored code (again) so all imports are in app.js

--- src/javascripts/vendor.js
+++
@@ -1,8 +0,0 @@
-import 'angular';
-import 'angular-ui-router';
-import 'ui-router-extras';
-import 'angular-spinner';
-import 'angular-color-picker';
-import 'angular-elastic-input';
-import 'angular-marked';
-import 'angular-vs-repeat';
