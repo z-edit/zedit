917d4f3db20d75a5a80bd1ec3cb483ca4c031b2c
Mator <why101@msn.com>
2017-11-23 12:15:25 -0800

    added newMerge function to mergeService

--- src/javascripts/Services/mergeService.js
+++ src/javascripts/Services/mergeService.js
@@ -1,3 +1,39 @@
-ngapp.service('mergeService', function() {
+ngapp.service('mergeService', function(settingsService) {
+    let getNewMergeName = function() {
+        let mergeName = 'New Merge',
+            counter = 1,
+            mergePath = settingsService.settings.mergePath,
+            directories = fh.getDirectories(mergePath).map(fh.getFileName);
+        while (directories.include(mergeName))
+            mergeName = `New Merge ${++counter}`;
+        return mergeName;
+    };
 
+    this.newMerge = function() {
+        let mergeName = getNewMergeName();
+        return {
+            name: mergeName,
+            filename: `${mergeName}.esp`,
+            plugins: [],
+            archives: [],
+            faceDataFiles: [],
+            voiceDataFiles: [],
+            scriptFragments: [],
+            stringFiles: [],
+            translations: [],
+            iniFiles: [],
+            seqFiles: [],
+            generalAssets: [],
+            extractArchives: false,
+            buildArchive: false,
+            handleFaceData: true,
+            handleVoiceData: true,
+            handleScriptFragments: true,
+            handleStringFiles: true,
+            handleTranslations: true,
+            handleIniFiles: true,
+            handleSeqFiles: true,
+            copyGeneralAssets: false
+        }
+    };
 });
