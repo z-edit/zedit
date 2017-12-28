35cad5f5406b0d55996189d0e98085c93ed02394
Mator <why101@msn.com>
2017-12-27 16:37:52 -0800

    added logic for building merge data plan when user goes to the data tab (could be improved)

--- src/javascripts/Views/merge/editMergeModal.js
+++ src/javascripts/Views/merge/editMergeModal.js
@@ -1,4 +1,4 @@
-ngapp.controller('editMergeModalController', function($scope, mergeService, tabsFactory) {
+ngapp.controller('editMergeModalController', function($scope, mergeService, mergeDataService, progressService) {
     // initialization
     $scope.editing = $scope.modalOptions.hasOwnProperty('merge');
     $scope.merge = $scope.modalOptions.merge || mergeService.newMerge();
@@ -7,5 +7,15 @@ ngapp.controller('editMergeModalController', function($scope, mergeService, tabs
     $scope.createMerge = function() {
         $scope.modalOptions.merges.push($scope.merge);
         $scope.$emit('closeModal');
+    };
+
+    $scope.buildMergeData = function() {
+        if ($scope.merge.hasData) return;
+        progressService.showProgress({
+            determinate: false,
+            message: 'Detecting assets...'
+        });
+        mergeDataService.buildMergeData($scope.merge);
+        progressService.hideProgress();
     };
 });
