bea353d71447e4e76286e9a05fe157dab5668fab
Mator <why101@msn.com>
2018-01-02 16:10:04 -0800

    expandable section now toggles between fa-plus and fa-minus icon when expanded/contracted

--- src/javascripts/Directives/expandableSection.js
+++ src/javascripts/Directives/expandableSection.js
@@ -8,10 +8,13 @@ ngapp.directive('expandableSection', function() {
         templateUrl: 'directives/expandableSection.html',
         link: function(scope, element) {
             let titleElement = element[0].children[0],
+                iconElement = titleElement.children[0],
                 contentElement = element[0].children[1];
 
             titleElement.addEventListener('click', function() {
                 contentElement.classList.toggle('ng-hide');
+                iconElement.classList.toggle('fa-plus');
+                iconElement.classList.toggle('fa-minus');
             });
         }
     }
