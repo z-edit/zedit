b4aa298561dcc196e893b1ea0bf17359da8b9fc3
Mator <why101@msn.com>
2018-01-11 13:40:17 -0800

    simplified edit merge modal buttons

--- src/javascripts/Views/merge/editMergeModal.js
+++ src/javascripts/Views/merge/editMergeModal.js
@@ -1,7 +1,8 @@
 ngapp.controller('editMergeModalController', function($scope, mergeService) {
     // scope functions
-    $scope.createMerge = function() {
-        $scope.modalOptions.merges.push($scope.merge);
+    $scope.save = function() {
+        if (!$scope.editing) $scope.modalOptions.merges.push($scope.merge);
+        mergeService.updateStatus($scope.merge);
         $scope.$emit('closeModal');
     };
 
