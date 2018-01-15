e848459967529157ec3f05aebd2bef892c968da3
Mator <why101@msn.com>
2018-01-14 17:48:26 -0800

    now setting plugin title properties based on their status, and fixed/finished some merge status stuff

--- src/javascripts/Services/merge/mergeStatusService.js
+++ src/javascripts/Services/merge/mergeStatusService.js
@@ -1,21 +1,25 @@
-ngapp.service('mergeStatusService', function($rootScope) {
-    let pluginChanged = function(plugin) {
+ngapp.service('mergeStatusService', function($rootScope, settingsService) {
+    let getOldPlugin = function(merge, filename) {
+        if (!merge.oldPlugins) return;
+        return merge.oldPlugins.findByKey('filename', filename);
+    };
+
+    let pluginChanged = function(merge, plugin) {
         let oldPlugin = getOldPlugin(merge, plugin.filename);
-        if (!oldPlugin) return true;
-        if (plugin.hash !== oldPlugin.hash) return true;
-        //if (plugin.)
+        return !oldPlugin || plugin.hash !== oldPlugin.hash ||
+            plugin.dataFolder !== oldPlugin.dataFolder;
     };
 
     let pluginsChanged = function(merge) {
         return merge.plugins.filter(function(plugin) {
-            plugin.changed = pluginChanged(plugin);
+            plugin.changed = pluginChanged(merge, plugin);
             return plugin.changed;
         }).length > 0;
     };
 
     let pluginAvailable = function(filename) {
         let plugin = $rootScope.loadOrder.findByKey('filename', filename);
-        return plugin && plugin.available;
+        return plugin && !plugin.disabled;
     };
 
     let allPluginsAvailable = function(merge) {
@@ -25,14 +29,35 @@ ngapp.service('mergeStatusService', function($rootScope) {
         }).length === 0;
     };
 
+    let mergedPluginExists = function(merge) {
+        let mergePath = settingsService.settings.mergePath,
+            path = `${mergePath}\\${merge.name}\\${merge.filename}`;
+        return fh.jetpack.exists(path);
+    };
+
+    let upToDate = function(merge) {
+        return mergedPluginExists(merge) && !pluginsChanged(merge);
+    };
+
     let getStatus = function(merge) {
-        if (merge.built) return 'Built';
         if (!allPluginsAvailable(merge)) return 'Plugins unavailable';
-        if (!pluginsChanged(merge)) return 'Up to date';
+        if (upToDate(merge)) return 'Up to date';
         return 'Ready to be built';
+    };
+
+    let getPluginTitle = function(plugin) {
+        if (!plugin.available) return 'Plugin unavailable to load.';
+        if (plugin.changed) return 'Plugin changed or just added to merge.';
+    };
+
+    let updatePluginTitles = function(merge) {
+        merge.plugins.forEach(function(plugin) {
+            plugin.title = getPluginTitle(plugin);
+        });
     };
 
     this.updateStatus = function(merge) {
         merge.status = getStatus(merge);
+        updatePluginTitles(merge);
     };
 });
