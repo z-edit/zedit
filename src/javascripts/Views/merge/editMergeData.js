8b0cee2f79feaf65e0d880ec6b7d79bf5890f5eb
Mator <why101@msn.com>
2018-01-14 17:47:09 -0800

    added plugin data folders section to the edit merge data tab

--- src/javascripts/Views/merge/editMergeData.js
+++ src/javascripts/Views/merge/editMergeData.js
@@ -2,6 +2,7 @@ ngapp.controller('editMergeDataController', function($scope, mergeDataService, p
     let assetArrayNames = ['archives', 'faceDataFiles', 'voiceDataFiles',
         'scriptFragments', 'stringFiles', 'translations', 'iniFiles'];
 
+    // helper functions
     let buildMergeData = function() {
         if ($scope.merge.hasData) return;
         progressService.showProgress({ message: 'Detecting assets...' });
@@ -13,6 +14,24 @@ ngapp.controller('editMergeDataController', function($scope, mergeDataService, p
         $scope.noAssets = assetArrayNames.reduce(function(b, key) {
             return b && $scope.merge[key].length === 0;
         }, true);
+    };
+
+    // scope functions
+    $scope.setDataFolder = function(plugin) {
+        let filename = plugin.filename;
+        if (!fh.jetpack.exists(`${plugin.dataFolder}\\${filename}`)) {
+            plugin.dataFolder = mergeDataService.getPluginDataFolder(filename);
+            return;
+        }
+        mergeDataService.setPluginDataFolder(filename, plugin.dataFolder);
+    };
+
+    $scope.browseDataFolder = function(plugin) {
+        let title = `Select data folder for ${plugin.filename}`,
+            newPath = fh.selectDirectory(title, plugin.dataFolder);
+        if (!newPath) return;
+        plugin.dataFolder = newPath;
+        $scope.setDataFolder(plugin);
     };
 
     // initialization
