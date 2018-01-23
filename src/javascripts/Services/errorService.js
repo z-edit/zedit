1e1d2436001d5a54b778805a25958c852d19c236
Mator <why101@msn.com>
2018-01-22 17:23:37 -0800

    error messages now include stack information

--- src/javascripts/Services/errorService.js
+++ src/javascripts/Services/errorService.js
@@ -2,7 +2,7 @@ ngapp.service('errorService', function() {
     let service = this;
 
     // default exception handler, should be overridden by view
-    this.handleException = (exception) => alert(exception);
+    this.handleException = (exception) => alert(exception.stack);
 
     this.try = function(callback) {
         try { callback() } catch(x) { service.handleException(x) }
