c726cd2a08a2d13902992ae048a93053cd8b9fa9
Colin <why101@msn.com>
2018-02-27 12:36:57 -0800

    added more functionality to mergeBuilder

--- src/javascripts/Services/merge/mergeBuilder.js
+++ src/javascripts/Services/merge/mergeBuilder.js
@@ -1,9 +1,7 @@
-ngapp.service('mergeBuilder', function(recordMergingService, mergeDataService) {
-    // INITIALIZATION
-    let loadPlugins = function(merge) {
-        // TODO: Move to its own service?
-    };
+ngapp.service('mergeBuilder', function(recordMergingService, mergeAssetHandler, pluginLoadService) {
+    const mastersPath = 'File Header\\Master Files';
 
+    // INITIALIZATION
     let createMergedPlugin = function(merge) {
         merge.plugin = xelib.AddFile(merge.filename);
     };
@@ -14,45 +12,46 @@ ngapp.service('mergeBuilder', function(recordMergingService, mergeDataService) {
         });
     };
 
+    let removePluginMasters = function(merge) {
+        let masters = xelib.GetElements(merge.plugin, mastersPath);
+        merge.plugins.forEach(function(pluginObj) {
+            xelib.RemoveArrayItem(masters, '', 'MAST', pluginObj.filename);
+        });
+    };
+
     let prepareMerge = function(merge) {
-        loadPlugins(merge);
+        pluginLoadService.loadPlugins(merge);
         createMergedPlugin(merge);
         addMastersToMergedPlugin(merge);
     };
 
-    // RECORD MERGING
-    let mergeRecords = function(merge) {
+    // FINALIZATION
+    let cleanMerge = function(merge) {
         if (merge.method === 'refactor') {
-            recordMergingService.refactorRecords(merge);
+            xelib.CleanMasters(merge.plugin);
         } else {
-            recordMergingService.clampRecords(merge);
+            removePluginMasters(merge);
         }
     };
 
-    // FINALIZATION
-    let cleanMerge = function(merge) {
-
-    };
-
     let saveMergeFiles = function(merge) {
-
-    };
-
-    let unloadPlugins = function(merge) {
-
+        fh.jetpack.dir(merge.dataPath);
+        let filePath = `${merge.dataPath}\\${merge.filename}`;
+        xelib.SaveFile(merge.plugin, filePath);
+        // TODO: save form ID map and other log stuff
     };
 
     let finalizeMerge = function(merge) {
         cleanMerge(merge);
         saveMergeFiles(merge);
-        unloadPlugins(merge);
+        pluginLoadService.unloadPlugins(merge);
     };
 
     // PUBLIC API
     this.buildMerge = function(merge) {
         prepareMerge(merge);
-        mergeRecords(merge);
-        handleAssets(merge);
+        recordMergingService.mergeRecords(merge);
+        mergeAssetHandler.handleAssets(merge);
         finalizeMerge(merge);
     };
 });
