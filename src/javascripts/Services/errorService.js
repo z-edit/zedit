2dcda666c304e179f6b3bc3d749f85e349478e18
Colin <why101@msn.com>
2018-05-06 14:22:29 -0700

    errorService now delegates to $exceptionHandler by default

--- src/javascripts/Services/errorService.js
+++ src/javascripts/Services/errorService.js
@@ -1,8 +1,7 @@
-ngapp.service('errorService', function() {
+ngapp.service('errorService', function($exceptionHandler) {
     let service = this;
 
-    // default exception handler, should be overridden by view
-    this.handleException = (exception) => alert(exception.stack);
+    this.handleException = $exceptionHandler;
 
     this.try = function(callback) {
         try { callback() } catch(x) { service.handleException(x) }
