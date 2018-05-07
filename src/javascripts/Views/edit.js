b5fc94831814d888ca8117ddf0c04a03229a313f
Colin <why101@msn.com>
2018-05-07 15:33:04 -0700

    now ignoring mouse input while the pluginLoader is active

--- src/javascripts/Views/edit.js
+++ src/javascripts/Views/edit.js
@@ -6,7 +6,7 @@ ngapp.config(['$stateProvider', function ($stateProvider) {
     });
 }]);
 
-ngapp.controller('editController', function ($scope, layoutService, hotkeyService, viewFactory) {
+ngapp.controller('editController', function ($scope, $timeout, layoutService, hotkeyService, viewFactory) {
     let getPluginItem = function(file) {
         return {
             handle: file,
@@ -38,12 +38,20 @@ ngapp.controller('editController', function ($scope, layoutService, hotkeyServic
         return filterView;
     };
 
-    // scope functions
+    let ignore = function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+    };
+
+    // event handlers
+    document.addEventListener('mousedown', ignore, true);
+    document.addEventListener('click', ignore, true);
+    document.addEventListener('mouseup', ignore, true);
+
     $scope.onViewportRender = function() {
         if (verbose) logger.info('Rendering viewport...');
     };
 
-    // event handlers
     $scope.$on('settingsClick', function() {
         if (!$scope.loaded) return;
         $scope.$emit('openModal', 'settings');
@@ -80,6 +88,12 @@ ngapp.controller('editController', function ($scope, layoutService, hotkeyServic
     $scope.$on('filesLoaded', function() {
         if (verbose) logger.info('Rendering editView');
         $scope.loaded = true;
+        $timeout(() => {
+            logger.info('Allowing click events');
+            document.removeEventListener('mousedown', ignore, true);
+            document.removeEventListener('click', ignore, true);
+            document.removeEventListener('mouseup', ignore, true);
+        }, 500);
     });
 
     // handle hotkeys
