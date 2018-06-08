95a18419eaa51325cac2258b31270f327620f0fd
Colin <why101@msn.com>
2018-06-08 15:35:36 -0700

    slight tweak to some parameter names

--- src/javascripts/Services/shared/interApiService.js
+++ src/javascripts/Services/shared/interApiService.js
@@ -2,16 +2,16 @@ ngapp.service('interApiService', function() {
     let registry = [];
 
     // PRIVATE
-    let excludeApi = function(key, entry) {
-        return entry.hasOwnProperty('except') && entry.except.includes(key) ||
-            entry.hasOwnProperty('only') && !entry.only.includes(key);
+    let excludeApi = function(api, entry) {
+        return entry.hasOwnProperty('except') && entry.except.includes(api) ||
+            entry.hasOwnProperty('only') && !entry.only.includes(api);
     };
 
     // PUBLIC API
-    this.getApi = function(key) {
+    this.getApi = function(apiKey) {
         let api = {};
         registry.forEach(entry => {
-            if (excludeApi(key, entry)) return;
+            if (excludeApi(apiKey, entry)) return;
             Object.assign(api, entry.api);
         });
         return api;
