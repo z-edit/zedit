c36b9bf3404c3d66e2fd375731ad2f72d857a798
Colin <why101@msn.com>
2018-08-07 16:41:12 -0700

    added basic detection code for nexus mod manager

--- src/javascripts/Services/shared/nexusModManagerService.js
+++ src/javascripts/Services/shared/nexusModManagerService.js
@@ -1,13 +1,29 @@
-ngapp.service('nexusModManagerService', function() {
-    this.getModManagerPath = function(settings) {
+ngapp.service('nexusModManagerService', function($rootScope, fileSearchService, profileService) {
+    let service = this;
 
+    this.getModManagerPath = function() {
+        return fileSearchService.findFile(/^NexusClient\.exe$/, {
+            initialQueue: [
+                process.env.USERPROFILE,
+                `${process.env.USERPROFILE}\\My Documents`,
+                `${process.env.USERPROFILE}\\Desktop`
+            ].map(path => ({ path, priority: 1 })),
+            prioritize: [/nexus/i, /^nmm$/i, /mod/i, /manager/i,
+                /tool/i, /gam(e|ing)/i, /program/i]
+        });
     };
 
-    this.getModsPath = function(settings) {
-
+    this.getModsPath = function(managerPath) {
+        if (!managerPath) return '';
+        let game = profileService.getGame($rootScope.profile.gameMode),
+            gameName = game.shortName,
+            modsPath = `${managerPath}\\${gameName}\\Mods\\VirtualInstall`;
+        return fh.jetpack.exists(modsPath) ? modsPath : '';
     };
 
     this.detect = function(settings) {
-
+        settings.managerPath = service.getModManagerPath();
+        settings.modsPath = service.getModsPath(settings.managerPath);
+        return settings.managerPath !== '';
     };
 });
