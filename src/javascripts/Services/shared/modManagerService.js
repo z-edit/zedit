69559ed7f2336556427b70d3f4894628ea70c1ce
Colin <why101@msn.com>
2018-08-07 15:51:00 -0700

    created modManagerService

---
+++ src/javascripts/Services/shared/modManagerService.js
@@ -0,0 +1,25 @@
+ngapp.service('modManagerService', function(nexusModManagerService, vortexService, modOrganizerService) {
+    let service = this;
+
+    this.modManagers = [{
+        name: 'Mod Organizer 2',
+        show: ['managerPath', 'modsPath', 'moInstance'],
+        detect: modOrganizerService.detect
+    }, {
+        name: 'Mod Organizer',
+        show: ['managerPath', 'modsPath'],
+        detect: modOrganizerService.detect
+    }, {
+        name: 'Vortex',
+        show: ['managerPath', 'modsPath'],
+        detect: vortexService.detect
+    }, {
+        name: 'Nexus Mod Manager',
+        show: ['managerPath', 'modsPath'],
+        detect: nexusModManagerService.detect
+    }];
+
+    this.addModManager = function(manager) {
+        service.modManagers.push(manager);
+    };
+});
