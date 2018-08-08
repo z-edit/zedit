616797255487240b5d292e5281004230f6d61b44
Colin <why101@msn.com>
2018-08-07 17:52:19 -0700

    added basic vortex path detection

--- src/javascripts/Services/shared/vortexService.js
+++ src/javascripts/Services/shared/vortexService.js
@@ -1,13 +1,29 @@
-ngapp.service('vortexService', function() {
-    this.getModManagerPath = function(settings) {
+ngapp.service('vortexService', function($rootScope, fileSearchService, profileService) {
+    let service = this;
 
+    this.getModManagerPath = function() {
+        return fileSearchService.findFile(/^Vortex\.exe$/, {
+            initialQueue: [
+                process.env.USERPROFILE,
+                `${process.env.USERPROFILE}\\My Documents`,
+                `${process.env.USERPROFILE}\\Desktop`
+            ].map(path => ({ path, priority: 2 })),
+            prioritize: [/vortex/i, /gam(e|ing)/i, /program/i, /mod/i, /tool/i,
+                /steam/i, /^common$/, /(Skyrim|Fallout|Oblivion)/i]
+        });
     };
 
-    this.getModsPath = function(settings) {
-
+    this.getModsPath = function() {
+        let game = profileService.getGame($rootScope.profile.gameMode),
+            gameName = game.shortName,
+            documentsPath = `${process.env.USERPROFILE}\\My Documents`,
+            modsPath = `${documentsPath}\\mods\\${gameName}`;
+        return fh.jetpack.exists(modsPath) ? modsPath : '';
     };
 
     this.detect = function(settings) {
-
+        settings.managerPath = service.getModManagerPath();
+        settings.modsPath = service.getModsPath();
+        return settings.managerPath !== '';
     };
 });
