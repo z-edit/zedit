709369d7ccc70d22a14a55fb25bf9bbd810f97b4
Colin <why101@msn.com>
2018-08-08 16:26:45 -0700

    made disable plugins merge integration

--- src/javascripts/Services/merge/mergeIntegrationService.js
+++ src/javascripts/Services/merge/mergeIntegrationService.js
@@ -6,8 +6,41 @@ ngapp.service('mergeIntegrationService', function(settingsService, modManagerSer
         fn && fn(merge);
     };
 
+    let pluginNotInMerge = function(merge, newFormat) {
+        let prefix = newFormat ? '*' : '';
+        let pluginFilenames = merge.plugins.map(obj => {
+            return prefix + obj.filename.toLowerCase();
+        });
+        return line => {
+            line = line.toLowerCase();
+            return !pluginFilenames.find(filename => line === filename);
+        };
+    };
+
+    let disablePluginsInMerge = function(merge) {
+        let notInMerge = pluginNotInMerge(merge, true);
+        return line => {
+            if (notInMerge(line)) return line;
+            return line.replace(/^\*/, '');
+        };
+    };
+
+    let getNewPluginsText = function(merge, pluginsText) {
+        let pluginLines = pluginsText.split('\r\n'),
+            gameAppName = xelib.GetGlobal('AppName'),
+            usingOldFormat = oldFormatGames.includes(gameAppName);
+        let newPluginLines = usingOldFormat ?
+            pluginLines.filter(pluginNotInMerge(merge)) :
+            pluginLines.map(disablePluginsInMerge(merge));
+        return newPluginLines.join('\r\n');
+    };
+
     this.disablePlugins = function(merge) {
-        // TODO
+        let appDataPath = xelib.GetGlobal('AppDataPath'),
+            pluginsPath = appDataPath + 'plugins.txt',
+            pluginsText = fh.loadTextFile(pluginsPath),
+            newText = getNewPluginsText(merge, pluginsText);
+        fh.saveTextFile(pluginsPath, newText);
     };
 
     this.runIntegrations = function(merge) {
