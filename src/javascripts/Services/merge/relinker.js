da74e7f65e35cafb745d9c93996a4e3b08b1dc08
Colin <why101@msn.com>
2018-09-13 14:17:35 -0700

    most relinker code complete, just need the final fixFileRefs function

--- src/javascripts/Services/merge/relinker.js
+++ src/javascripts/Services/merge/relinker.js
@@ -1,4 +1,5 @@
-ngapp.service('relinker', function(scriptsCache, pexService) {
+ngapp.service('relinker', function(scriptsCache, bsaHelpers, pexService, settingsService) {
+    let dataPath;
 
     let getMergedPlugins = function(merges) {
         let mergedPlugins = {};
@@ -14,21 +15,38 @@ ngapp.service('relinker', function(scriptsCache, pexService) {
         let cache = scriptsCache.update(),
             mergedPlugins = getMergedPlugins(merges);
         return cache.filter(entry => {
-            return !!entry.fileRefs.find(ref => {
-                return mergedPlugins.hasOwnProperty(ref.plugin);
+            return !!entry.fileRefs.find(file => {
+                return mergedPlugins.hasOwnProperty(file);
             });
         });
     };
 
+    let getDataPath = function() {
+        if (!dataPath) dataPath = xelib.GetGlobal('DataPath');
+        return dataPath;
+    };
+
+    let getScriptFilePath = function(entry) {
+        let basePath = `scripts\\${entry.filename}`;
+        return entry.bsa ? bsaHelpers.extractFile(entry.bsa, basePath) :
+            `${getDataPath()}\\${basePath}`;
+    };
+
+    let fixFileRefs = function(script, merges) {
+        // TODO
+    };
+
     this.relinkScripts = function(merges) {
-        let scripts = getScriptsToRelink(merges);
-        scripts.forEach(script => {
-            // 1: get script filePath,
-            // 1A: extract if necessary
-            // 1B: exit if does not exist
-            // 2: load script using pexService
-            // 3: iterate through calls, fix file refs
-            // 4: save script
+        let scripts = getScriptsToRelink(merges),
+            {relinkerPath} = settingsService.settings;
+        fh.jetpack.dir(`${relinkerPath}\\scripts`);
+        scripts.forEach(entry => {
+            let filePath = getScriptFilePath(entry);
+            if (!filePath || !fh.jetpack.exists(filePath)) return;
+            let script = pexService.loadScript(filePath),
+                newPath = `${relinkerPath}\\scripts\\${entry.filename}`;
+            fixFileRefs(script, merges);
+            pexService.saveScript(script, newPath);
         });
     };
 });
