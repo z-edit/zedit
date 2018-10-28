863588deb7c9726c354e7abc72e0d7a4458e1ae8
Colin <why101@msn.com>
2018-10-28 16:21:41 -0700

    limited plugins listed in edit merge plugins warning dialog

--- src/javascripts/Views/merge/editMergeModal.js
+++ src/javascripts/Views/merge/editMergeModal.js
@@ -1,7 +1,14 @@
 ngapp.controller('editMergeModalController', function($scope, mergeService, mergeStatusService) {
-    let pluginWarning = function() {
-        let pluginsStr = $scope.merge.warningPlugins
+    let getWarningString = function(warningPlugins) {
+        return (warningPlugins.length > 6 ?
+            warningPlugins.slice(0, 6).concat([
+                `... (${warningPlugins.length - 6} more)`
+            ]) : warningPlugins)
             .reduce((str, filename) => str + `- ${filename}\n`, '');
+    };
+
+    let pluginWarning = function() {
+        let pluginsStr = getWarningString($scope.merge.warningPlugins);
         return confirm(`The following plugins will not be usable after building this merge.  You can include them in the merge or remove the plugins they require from the merge to resolve this.  Proceed anyways? \n\nPlugins:\n${pluginsStr}`);
     };
 
