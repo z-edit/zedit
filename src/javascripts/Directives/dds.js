94472b0ca9d427628371064b6bd52e325742e672
Colin <why101@msn.com>
2019-01-09 02:11:13 -0800

    fixed getting and applying image data to dds

--- src/javascripts/Directives/dds.js
+++ src/javascripts/Directives/dds.js
@@ -9,11 +9,15 @@ ngapp.directive('dds', function() {
             let canvas = element[0].firstElementChild,
                 context = canvas.getContext('2d');
 
+            canvas.width = 512;
+            canvas.height = 512;
+
             scope.$watch('texturePath', function() {
                 context.clearRect(0, 0, canvas.width, canvas.height);
-                let texturePath = `textures\\${scope.texturePath}`;
-                if (fh.jetpack.exists(texturePath) !== 'file') return;
-                let imageData = xelib.GetBitmapResource(texturePath);
+                element[0].title = scope.texturePath;
+                if (!scope.texturePath) return;
+                let texturePath = `textures\\${scope.texturePath}`,
+                    imageData = xelib.GetBitmapResource(texturePath);
                 context.putImageData(imageData, 0, 0);
             });
         }
