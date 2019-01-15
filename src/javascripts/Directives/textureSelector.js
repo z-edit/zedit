99ad550006a1f92a9e9e99e6e479ab939f62e676
Colin <why101@msn.com>
2019-01-14 20:26:43 -0800

    added texture selector directive

---
+++ src/javascripts/Directives/textureSelector.js
@@ -0,0 +1,37 @@
+ngapp.directive('textureSelector', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            texture: '=',
+            key: '@'
+        },
+        templateUrl: 'directives/textureSelector.html',
+        controller: 'textureSelectorController'
+    }
+});
+
+ngapp.controller('textureSelectorController', function($scope, $timeout, recentService, resourceService, hotkeyService) {
+    recentService.store($scope.key, 10);
+    $scope.recentTextures = recentService.get($scope.key);
+
+    // inherited functions
+    hotkeyService.buildOnKeyDown($scope, 'onInputKeyDown', 'customSelect');
+
+    // scope functions
+    $scope.textureSearch = function(text) {
+        const texturesLength = 9;
+        return resourceService.getFiles('textures')
+            .filter(path => path.contains(text, true))
+            .slice(0, 100)
+            .map(path => path.slice(texturesLength));
+    };
+
+    $scope.setTexture = texture => $scope.texture = texture;
+
+    $scope.setCustomTexture = function(texture) {
+        $scope.$applyAsync(() => {
+            $scope.texture = texture;
+            recentService.add($scope.key, $scope.texture);
+        });
+    };
+});
