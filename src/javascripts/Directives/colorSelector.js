32bd0d0ad3702122fb43c7649123bfd296920df6
Colin <why101@msn.com>
2019-01-20 20:27:35 -0800

    added dropover, color selector now uses drop over

--- src/javascripts/Directives/colorSelector.js
+++ src/javascripts/Directives/colorSelector.js
@@ -2,39 +2,23 @@ ngapp.directive('colorSelector', function() {
     return {
         restrict: 'E',
         scope: {
-            color: '=',
-            key: '@'
+            color: '='
         },
         templateUrl: 'directives/colorSelector.html',
         controller: 'colorSelectorController'
     }
 });
 
-ngapp.controller('colorSelectorController', function($scope, recentService) {
-    recentService.store($scope.key, 11);
-    $scope.recentColors = recentService.get($scope.key);
-
-    $scope.setColor = function(color) {
+ngapp.controller('colorSelectorController', function($scope) {
+    $scope.selectColor = function(color) {
         $scope.$applyAsync(() => {
             color = new Color(color);
             $scope.color.copyRGB(color);
-            recentService.add($scope.key, $scope.color);
         });
     };
 
     $scope.$watch('color', () => {
         if (!$scope.color) return;
         $scope.customColorText = $scope.color.toRGB();
-    });
-
-    $scope.$watch('recentColors', () => {
-        let numEntries = $scope.recentColors.length + 1;
-        $scope.width = {
-            1: '75',
-            2: '150',
-            3: '225',
-            4: '150',
-            5: '225'
-        }[Math.min(numEntries, 5)];
     }, true);
 });
---
+++ src/javascripts/Directives/dropover.js
@@ -0,0 +1,43 @@
+ngapp.directive('dropover', function() {
+    return {
+        restrict: 'E',
+        transclude: {
+            display: 'display',
+            drop: 'drop'
+        },
+        scope: {
+            callback: '='
+        },
+        templateUrl: 'directives/dropover.html',
+        controller: 'dropoverController',
+        link: function(scope, element) {
+            element[0].setAttribute('tabindex', '0');
+            element[0].addEventListener('keydown', function(e) {
+                scope.$applyAsync(() => scope.onDropoverKeyDown(e));
+            });
+        }
+    }
+});
+
+ngapp.controller('dropoverController', function($scope, hotkeyService) {
+    // inherited functions
+    hotkeyService.buildOnKeyDown($scope, 'onDropoverKeyDown', 'dropover');
+
+    // scope functions
+    $scope.toggleDropover = function() {
+        $scope.showDropover = !$scope.showDropover;
+    };
+
+    $scope.hideDropover = function() {
+        $scope.showDropover = false;
+    };
+
+    $scope.select = function() {
+        $scope.callback($scope.customItem);
+        $scope.hideDropover();
+    };
+
+    // event handlers
+    $scope.$on('handleEnter', $scope.select);
+    $scope.$on('handleEscape', $scope.hideDropover);
+});
