a2dde8942e9bb66ad5cc775dca9d0714bb0c47d8
Colin <why101@msn.com>
2019-01-21 11:30:16 -0800

    added AppName, AppDataPath, and DataPath getters to gameService

--- src/javascripts/Services/shared/gameService.js
+++ src/javascripts/Services/shared/gameService.js
@@ -1,10 +1,6 @@
 ngapp.service('gameService', function() {
-    let bethesdaFiles = fh.loadResource('app/bethesdaFiles.json'),
-        appName;
-
-    let getAppName = function() {
-        appName = xelib.GetGlobal('AppName')
-    };
+    let {plugins, archives} = fh.loadResource('app/bethesdaFiles.json'),
+        appName, dataPath, appDataPath;
 
     let includesIgnoreCase = function(a, str) {
         str = str.toLowerCase();
@@ -13,12 +9,32 @@ ngapp.service('gameService', function() {
 
     // PUBLIC API
     this.isBethesdaPlugin = function(filename) {
-        if (!appName) getAppName();
-        return includesIgnoreCase(bethesdaFiles.plugins[appName], filename);
+        return includesIgnoreCase(plugins[this.appName], filename);
     };
 
     this.isBethesdaArchive = function(filename) {
         if (!appName) getAppName();
-        return includesIgnoreCase(bethesdaFiles.archives[appName], filename);
+        return includesIgnoreCase(archives[this.appName], filename);
     };
+
+    Object.defineProperty(this, 'dataPath', {
+        get: function() {
+            if (!dataPath) dataPath = xelib.GetGlobal('DataPath');
+            return dataPath;
+        }
+    });
+
+    Object.defineProperty(this, 'appName', {
+        get: function() {
+            if (!appName) appName = xelib.GetGlobal('AppName');
+            return appName;
+        }
+    });
+
+    Object.defineProperty(this, 'appDataPath', {
+        get: function() {
+            if (!appDataPath) appDataPath = xelib.GetGlobal('AppDataPath');
+            return appDataPath;
+        }
+    });
 });
