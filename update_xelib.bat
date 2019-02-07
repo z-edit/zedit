ecd7028fe3ecd335e20b5a19632f9c6506d5ba40
Colin <why101@msn.com>
2019-02-06 21:00:58 -0800

    updated xelib to v0.6.0

--- update_xelib.bat
+++ update_xelib.bat
@@ -1,5 +1,5 @@
-call npm i matortheeternal/xelib --save
-cd node_modules\xelib
-call npm i nan --save
-cd ..\..
+call npm i z-edit/xelib --save
+copy /Y .\node_modules\xelib\XEditLib.dll .\XEditLib.dll
+del ".\node_modules\xelib\XEditLib.dll"
+del ".\node_modules\xelib\*.dat"
 call npm run rebuild
