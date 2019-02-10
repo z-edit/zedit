7722aa1783fe1bd6c7010858ff3a122e793ebe3b
Colin <why101@msn.com>
2019-02-09 21:32:56 -0800

    refactored archive creation settings

---
+++ src/javascripts/Views/shared/archiveCreationSettings.js
@@ -0,0 +1,48 @@
+ngapp.controller('archiveCreationSettingsController', function($scope) {
+    $scope.addFileExpr = function() {
+        $scope.settings.archiveCreation.fileExprs.push('')
+    };
+
+    $scope.removeFileExpr = function(index) {
+        $scope.settings.archiveCreation.fileExprs.splice(index, 1);
+    };
+});
+
+ngapp.run(function($rootScope, settingsService, gameService) {
+    const GIGABYTE = 1024 * 1024 * 1024;
+
+    settingsService.registerSettings({
+        label: 'Archive Creation Settings',
+        templateUrl: `/partials/settings/archiveCreation.html`,
+        controller: 'archiveCreationSettingsController',
+        defaultSettings: {
+            archiveCreation: {
+                fileExprs: [
+                    'docs/**/*',
+                    'interface/**/*',
+                    'meshes/**/*',
+                    'lodsettings/**/*',
+                    'music/**/*',
+                    'scripts/*.pex',
+                    'scripts/source/*.psc',
+                    'seq/*.seq',
+                    'sound/**/*',
+                    'textures/**/*'
+                ],
+                createMultipleArchives: false,
+                minFileCount: 10
+            }
+        }
+    });
+
+    $rootScope.$on('sessionStarted', function() {
+        let {settings} = settingsService;
+        if (settings.archiveCreation.initialized) return;
+        Object.assign(settings.archiveCreation, {
+            initialized: true,
+            createTexturesArchive: gameService.appName === 'SSE',
+            maxSize: (gameService.appName === 'FO4' ? 16 : 2) * GIGABYTE
+        });
+        settingsService.saveProfileSettings();
+    });
+});
