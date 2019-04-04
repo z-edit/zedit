8c328c4773f7d019c09be59f85d280c3d25f9441
Colin <why101@msn.com>
2019-04-04 11:59:42 -0700

    added error display to edit merge load order tab

--- src/javascripts/Views/merge/editMergeLoadOrder.js
+++ src/javascripts/Views/merge/editMergeLoadOrder.js
@@ -1,13 +1,19 @@
-ngapp.controller('editMergeLoadOrderController', function($scope, $timeout, mergeLoadService) {
+ngapp.controller('editMergeLoadOrderController', function($rootScope, $scope, $timeout, mergeLoadService) {
     // helper functions
     let pluginInMerge = function(filename) {
         return !!$scope.merge.plugins.findByKey('filename', filename);
     };
 
+    let getMasterNames = function(filename) {
+        let plugin = $rootScope.loadOrder.findByKey('filename', filename);
+        return plugin.masterNames.slice();
+    };
+
     let buildLoadOrder = function() {
         $scope.loadOrder = $scope.merge.loadOrder.map(filename => ({
             filename: filename,
-            active: pluginInMerge(filename)
+            masterNames: getMasterNames(filename),
+            inMerge: pluginInMerge(filename)
         }));
     };
 
@@ -22,19 +28,79 @@ ngapp.controller('editMergeLoadOrderController', function($scope, $timeout, merg
         });
     };
 
+    let getOutOfOrderMasters = function() {
+        let filenames = [];
+        return $scope.loadOrder.reduce((obj, entry) => {
+            entry.masterNames.forEach(masterName => {
+                if (filenames.includes(masterName)) return;
+                if (!obj.hasOwnProperty(masterName))
+                    obj[masterName] = [];
+                obj[masterName].push(entry.filename);
+            });
+            filenames.push(entry.filename);
+            return obj;
+        }, {});
+    };
+
+    let updateOutOfOrderErrors = function() {
+        let outOfOrderMasters = getOutOfOrderMasters();
+        $scope.loadOrder.forEach(entry => {
+            if (!outOfOrderMasters.hasOwnProperty(entry.filename)) return;
+            entry.error = true;
+            let mastersStr = outOfOrderMasters[entry.filename]
+                .map(filename => `- ${filename}`)
+                .join('\n');
+            if (entry.title) entry.title += '\n';
+            entry.title += 'Error: This plugin is loaded after plugins that require it:\n' + mastersStr;
+        });
+    };
+
+    let notContiguous = function(entry) {
+        entry.error = true;
+        entry.title += 'Error: This plugin makes the merge not contiguous.';
+    };
+
+    let updateNotContiguousErrors = function() {
+        if ($scope.merge.method !== 'Clobber') return;
+        let mergeStarted = false;
+        $scope.loadOrder.forEach(entry => {
+            let inMerge = pluginInMerge(entry.filename);
+            if (mergeStarted && !inMerge) notContiguous(entry);
+            mergeStarted = mergeStarted || inMerge;
+        });
+    };
+
+    let resetErrors = function() {
+        $scope.loadOrder.forEach(entry => {
+            entry.error = false;
+            entry.title = '';
+        });
+    };
+
+    let updateErrors = function() {
+        resetErrors();
+        updateNotContiguousErrors();
+        updateOutOfOrderErrors();
+    };
+
     // event handlers
     $scope.$watch('merge.useGameLoadOrder', () => {
         if (!$scope.merge.useGameLoadOrder) return;
         mergeLoadService.resetMergeLoadOrder($scope.merge);
+        buildLoadOrder();
+        updateIndexes();
+        updateErrors();
     });
 
     $scope.$on('itemsReordered', function(e) {
-        $timeout(updateIndexes);
+        updateIndexes();
         updateLoadOrder();
+        updateErrors();
         e.stopPropagation();
     });
 
     // initialization
     buildLoadOrder();
     updateIndexes();
+    updateErrors();
 });
