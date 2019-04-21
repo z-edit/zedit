fe98480608bca050a88f9a2b0054de85f3393e85
Colin <why101@msn.com>
2019-04-21 11:24:37 -0700

    viewFactory.new now allows passing options

--- src/javascripts/Factories/viewFactory.js
+++ src/javascripts/Factories/viewFactory.js
@@ -29,13 +29,13 @@ ngapp.service('viewFactory', function(randomService) {
         return angular.copy(accessibleViews);
     };
 
-    this.new = function(viewName) {
-        return {
+    this.new = function(viewName, options) {
+        return Object.assign({
             templateUrl: `partials/${viewName}.html`,
             controller: `${viewName}Controller`,
             class: viewName.underscore('-'),
             label: viewName.humanize(),
             destroy: () => {}
-        };
+        }, options || {});
     }
 });
