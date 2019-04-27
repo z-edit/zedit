0a4a43b22b35940b4e0bb12d79ae1b49ad84a181
Colin <why101@msn.com>
2019-04-26 17:04:16 -0700

    added tryPromise to errorService

--- src/javascripts/Services/shared/errorService.js
+++ src/javascripts/Services/shared/errorService.js
@@ -15,4 +15,12 @@ ngapp.service('errorService', function($exceptionHandler) {
     this.tryEach = function(items, callback) {
         items.forEach(item => service.try(() => callback(item)));
     };
+
+    this.tryPromise = function(callback, onSuccess, onError) {
+        try {
+            callback().then(onSuccess, onError);
+        } catch (x) {
+            onError(x);
+        }
+    }
 });
