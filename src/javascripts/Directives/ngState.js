74b7c4bf60c975fdb40920cfb90323073fa0c29b
Colin <why101@msn.com>
2019-06-02 16:29:30 -0700

    ngState now stops propagation of double clicks on checkbox elements

--- src/javascripts/Directives/ngState.js
+++ src/javascripts/Directives/ngState.js
@@ -1,9 +1,13 @@
-ngapp.directive('ngState', function() {
+ngapp.directive('ngState', function(eventService) {
     const CHECKBOX_INDETERMINATE = 2;
 
     return {
         restrict: 'A',
         link: function(scope, element, attributes) {
+            eventService.handleEvents(scope, element[0], {
+                dblclick: e => e.stopPropagation()
+            });
+
             scope.$watch(attributes.ngState, value => {
                 element[0].checked = Boolean(value);
                 element[0].indeterminate = value === CHECKBOX_INDETERMINATE;
