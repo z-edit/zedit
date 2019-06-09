e302233d8fde6dd1ed8b52a8e13d65b1c1db547e
Colin <why101@msn.com>
2019-06-08 19:06:16 -0700

    added code for applying default rules to nodes in smash rules

--- src/javascripts/Services/smash/smashRecordRuleService.js
+++ src/javascripts/Services/smash/smashRecordRuleService.js
@@ -1,7 +1,46 @@
 ngapp.service('smashRecordRuleService', function() {
     let service = this;
 
+    let skipElementExpr = /(?:unknown|unused)/i,
+        veryLargeRecords = ['NAVI', 'NAVM', 'RACE'],
+        recordsMergedAtRuntime = ['DOBJ', 'LCTN', 'IDLE'],
+        entityTypes = [xelib.stUnsortedArray, xelib.stUnsortedStructArray],
+        deletionsTypes = [xelib.stSortedArray, xelib.stSortedStructArray];
+
+    // PRIVATE
+    let shouldSkipRecord = function(sig) {
+        return veryLargeRecords.includes(sig) ||
+            recordsMergedAtRuntime.includes(sig)
+    };
+
+    let shouldSkipElement = function(name) {
+        return skipElementExpr.test(name);
+    };
+
+    let shouldSkip = function(element) {
+        return element.type === xelib.stRecord ?
+            shouldSkipRecord(element.signature) :
+            shouldSkipElement(element.name);
+    };
+
+    let shouldSetEntity = function(element) {
+        return entityTypes.includes(element.type);
+    };
+
+    let shouldSetDeletions = function(element) {
+        return deletionsTypes.includes(element.type);
+    };
+
     // PUBLIC API
+    this.applyDefaultRules = function(element) {
+        if (shouldSkip(element)) return;
+        element.process = true;
+        if (shouldSetEntity(element)) element.entity = true;
+        if (shouldSetDeletions(element)) element.deletions = true;
+        if (!element.elements) return;
+        element.elements.forEach(service.applyDefaultRules);
+    };
+
     this.addRecord = function(records, sig) {
         if (records.hasOwnProperty(sig)) return;
         xelib.WithHandle(xelib.GetRecordDef(sig), def => {
