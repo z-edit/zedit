4db2964f0b09c9a5e8f89d27367b4d4054119e0e
Colin <why101@msn.com>
2019-10-27 10:45:55 -0700

    properly removed old gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
 require('./tasks/build_app');
-require('./tasks/build_tests');
 require('./tasks/start');
 require('./tasks/generateCommonDocs');
--- tasks/start.js
+++ tasks/start.js
@@ -4,11 +4,11 @@ const childProcess = require('child_process');
 const electron = require('electron');
 const gulp = require('gulp');
 
-gulp.task('start', gulp.series('build', gulp.parallel('watch', function app(done) {
+gulp.task('start', gulp.series('build', function app(done) {
     childProcess.spawn(electron, ['.'], {
         stdio: 'inherit'
     }).on('close', function () {
         process.exit();
         done();
     });
-})));
+}));
