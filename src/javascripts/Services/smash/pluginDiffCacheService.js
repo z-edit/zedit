a2a3b6b3c51e2d8a63644742dfc6d723bf29c598
Colin <why101@msn.com>
2019-12-17 21:02:54 -0800

    updated pluginDiffCacheService

--- src/javascripts/Services/smash/pluginDiffCacheService.js
+++ src/javascripts/Services/smash/pluginDiffCacheService.js
@@ -1,11 +1,13 @@
-ngapp.service('pluginDiffCacheService', function($rootScope, recordChangeService, progressLogger) {
+ngapp.service('pluginDiffCacheService', function($rootScope, recordChangeService, progressLogger, progressService, gameService) {
     let {WithEachHandle, GetRecords, IsOverride, Name,
             WithHandle, GetMasterRecord, GetElementFile} = xelib,
         {getRecordChanges} = recordChangeService,
-        {log, progress} = progressLogger;
+        {log, progress} = progressLogger,
+        {progressMessage} = progressService;
 
     let cache = {},
-        oldCache = {};
+        oldCache = {},
+        n, start;
 
     // private
     let getFileHash = function(filename) {
@@ -15,8 +17,8 @@ ngapp.service('pluginDiffCacheService', function($rootScope, recordChangeService
     };
 
     let getCacheFilePath = function(pluginFileName, hash) {
-        let filename = `${pluginFileName}-${hash}.json`;
-        return fh.path('cache', 'pluginDiffs', filename);
+        return fh.jetpack.dir('cache/pluginDiffs')
+            .path(`${pluginFileName}-${hash}.json`);
     };
 
     let getElementFileName = function(record) {
@@ -25,30 +27,49 @@ ngapp.service('pluginDiffCacheService', function($rootScope, recordChangeService
         });
     };
 
+    let rateMessage = function(n, start) {
+        let time = new Date() - start,
+            rate = (n / time) * 1000 * 60,
+            rateStr = `${rate.toFixed(0)} overrides/min`;
+        progressMessage(`Updating plugin diff cache (${rateStr})`);
+    };
+
     let buildCache = function(file, filename, filePath) {
         let cache = {};
         log(`Getting changes in ${filename}`);
-        WithEachHandle(GetRecords(file, true), rec => {
+        WithEachHandle(GetRecords(file, '', true), rec => {
             if (!IsOverride(rec)) return;
+            if (n++ % 50 === 49) rateMessage(n, start);
             WithHandle(GetMasterRecord(rec, file), masterRec => {
-                let masterName = getElementFileName(masterRec);
+                let masterName = getElementFileName(masterRec),
+                    changes = getRecordChanges(masterRec, rec);
+                if (!changes) return;
                 if (!cache.hasOwnProperty(masterName))
                     cache[masterName] = [];
-                cache[masterName].push(getRecordChanges(masterRec, rec));
+                cache[masterName].push(changes);
             });
         });
-        fh.saveJsonFile(filePath, cache);
+        fh.saveJsonFile(filePath, cache, true);
         return cache;
+    };
+
+    let loadCache = function(filename, filePath) {
+        if (!fh.fileExists(filePath)) return;
+        log(`Loading cached changes ${filePath}`);
+        return fh.loadJsonFile(filePath);
     };
 
     // public
     this.updateCache = function() {
         progress('Updating plugin diff cache');
+        n = 0;
+        start = new Date();
         xelib.WithEachHandle(xelib.GetElements(), file => {
-            let filename = xelib.Name(file),
-                hash = getFileHash(filename),
+            let filename = xelib.Name(file);
+            if (gameService.isGameEsmOrExe(filename)) return;
+            let hash = getFileHash(filename),
                 filePath = getCacheFilePath(filename, hash);
-            cache[filename] = fh.loadJsonFile(filePath) ||
+            cache[filename] = loadCache(filename, filePath) ||
                 buildCache(file, filename, filePath);
         });
     };
